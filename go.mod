d1150526bc76c8238f974ab5d43003508be13e0e
Roy O'Young <roy2220@outlook.com>
2020-09-29 22:37:29 +0800

    Add parser test

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/creack/pty v1.1.11
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
---
+++ parser_test.go
@@ -0,0 +1,254 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParser(t *testing.T) {
+	type FeedDataCall struct {
+		Data         string
+		OK           bool
+		CapturedData string
+		IgnoredData  string
+	}
+	for _, tt := range []struct {
+		CaptureBegin  string
+		CaptureEnd    string
+		FeedDataCalls []FeedDataCall
+	}{
+		{
+			CaptureBegin: "<<<",
+			CaptureEnd:   ">>>",
+			FeedDataCalls: []FeedDataCall{
+				{
+					Data:         "aaa<<<\x00123>>>ccc",
+					CapturedData: "",
+					IgnoredData:  "aaa",
+					OK:           false,
+				},
+			},
+		},
+		{
+			CaptureBegin: "<<<",
+			CaptureEnd:   ">>>",
+			FeedDataCalls: []FeedDataCall{
+				{
+					Data:         "aaa<<<123>>>ccc\000",
+					CapturedData: "123",
+					IgnoredData:  "aaa",
+					OK:           false,
+				},
+			},
+		},
+		{
+			CaptureBegin: "<++",
+			CaptureEnd:   "++>",
+			FeedDataCalls: []FeedDataCall{
+				{
+					Data:         "xxx<++HAHA++>yy<++YOYO++>z",
+					CapturedData: "HAHAYOYO",
+					IgnoredData:  "xxxyyz",
+					OK:           true,
+				},
+			},
+		},
+		{
+			CaptureBegin: "<--",
+			CaptureEnd:   "-->",
+			FeedDataCalls: []FeedDataCall{
+				{
+					Data:         "aaa",
+					IgnoredData:  "aaa",
+					CapturedData: "",
+					OK:           true,
+				},
+				{
+					Data:         "<-",
+					IgnoredData:  "aaa",
+					CapturedData: "",
+					OK:           true,
+				},
+				{
+					Data:         "-hello<--world-",
+					IgnoredData:  "aaa",
+					CapturedData: "",
+					OK:           true,
+				},
+				{
+					Data:         "->bbb",
+					CapturedData: "hello<--world",
+					IgnoredData:  "aaabbb",
+					OK:           true,
+				},
+			},
+		},
+	} {
+		t.Run(tt.CaptureBegin+"..."+tt.CaptureEnd, func(t *testing.T) {
+			capturedData := []byte(nil)
+			ignoredData := []byte(nil)
+			p := parser{
+				CaptureBegin: []byte(tt.CaptureBegin),
+				CaptureEnd:   []byte(tt.CaptureEnd),
+				CapturedDataHandler: func(data []byte) bool {
+					for i := range data {
+						if data[i] == '\x00' {
+							return false
+						}
+					}
+					capturedData = append(capturedData, data...)
+					return true
+				},
+				IgnoredDataHandler: func(data []byte) bool {
+					for i := range data {
+						if data[i] == '\x00' {
+							return false
+						}
+					}
+					ignoredData = append(ignoredData, data...)
+					return true
+				},
+			}
+			p.Init()
+			for _, fdc := range tt.FeedDataCalls {
+				ok := p.FeedData([]byte(fdc.Data))
+				assert.Equal(t, fdc.OK, ok)
+				assert.Equal(t, fdc.CapturedData, string(capturedData))
+				assert.Equal(t, fdc.IgnoredData, string(ignoredData))
+			}
+		})
+	}
+}
+
+func TestPattern(t *testing.T) {
+	type FindStopCall struct {
+		Data        string
+		SkippedData string
+		I           int
+		OK          bool
+	}
+	for _, tt := range []struct {
+		Pattern       string
+		FindStopCalls []FindStopCall
+	}{
+		{
+			Pattern: "1212A1212B",
+			FindStopCalls: []FindStopCall{
+				{
+					Data:        "1212",
+					I:           0,
+					OK:          false,
+					SkippedData: "",
+				},
+				{
+					Data:        "12A",
+					I:           0,
+					OK:          false,
+					SkippedData: "12",
+				},
+				{
+					Data:        "121",
+					I:           0,
+					OK:          false,
+					SkippedData: "12",
+				},
+				{
+					Data:        "2A",
+					I:           0,
+					OK:          false,
+					SkippedData: "121212A",
+				},
+				{
+					Data:        "1212B",
+					I:           5,
+					OK:          true,
+					SkippedData: "121212A",
+				},
+			},
+		},
+		{
+			Pattern: "123123abc",
+			FindStopCalls: []FindStopCall{
+				{
+					Data:        "bbb---",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---",
+				},
+				{
+					Data:        "123",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---",
+				},
+				{
+					Data:        "123",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---",
+				},
+				{
+					Data:        "abc",
+					I:           3,
+					OK:          true,
+					SkippedData: "bbb---",
+				},
+				{
+					Data:        "+++eee",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---+++eee",
+				},
+				{
+					Data:        "123",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---+++eee",
+				},
+				{
+					Data:        "123",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---+++eee",
+				},
+				{
+					Data:        "123",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---+++eee123",
+				},
+				{
+					Data:        "ab",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---+++eee123",
+				},
+				{
+					Data:        "c",
+					I:           1,
+					OK:          true,
+					SkippedData: "bbb---+++eee123",
+				},
+				{
+					Data:        "de",
+					I:           0,
+					OK:          false,
+					SkippedData: "bbb---+++eee123de",
+				},
+			},
+		},
+	} {
+		t.Run(tt.Pattern, func(t *testing.T) {
+			p := pattern{Raw: []byte(tt.Pattern)}
+			p.Init()
+			skippedData := []byte(nil)
+			for _, fsc := range tt.FindStopCalls {
+				i, ok := p.FindStop([]byte(fsc.Data), &skippedData)
+				assert.Equal(t, fsc.I, i)
+				assert.Equal(t, fsc.OK, ok)
+				assert.Equal(t, fsc.SkippedData, string(skippedData))
+			}
+		})
+	}
+}
