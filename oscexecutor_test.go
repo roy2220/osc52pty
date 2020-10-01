04f0bf1cf1852dc1b16815066118855b2c331d58
Roy O'Young <roy2220@outlook.com>
2020-10-01 17:45:10 +0800

    Fix TestSetClipboard

--- oscexecutor_test.go
+++ oscexecutor_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/base64"
 	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,7 +14,7 @@ func TestSetClipboard(t *testing.T) {
 	if !assert.NoError(t, err) {
 		t.FailNow()
 	}
-	// defer os.Remove(tempFile.Name())
+	defer os.Remove(tempFile.Name())
 	text := "hello clipboard\n"
 	encodedText := base64.StdEncoding.EncodeToString([]byte(text))
 	err = setClipboard([]string{"tee", tempFile.Name()}, []byte(encodedText))
